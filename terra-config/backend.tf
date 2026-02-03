c11e1736a907781229f4acdebf530ddac7da3a19
Himanshu Toshniwal <164766157+Himanshu-Toshniwal@users.noreply.github.com>
2026-02-03 09:12:25 +0530

    Update S3 backend bucket and region

--- terra-config/backend.tf
+++ terra-config/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "pravesh-tetris-backend"
+    bucket = "himanshu-tetris-backend"
     key    = "ecs-state-file/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
